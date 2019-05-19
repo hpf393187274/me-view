b019ba8ac45e4059b4749dfacc36b15373106ce5
hpf9527 <393187274@qq.com>
2019-05-19 20:16:04 +0800

    测试数据

--- src/demo/data/tree.js
+++ src/demo/data/tree.js
@@ -26,47 +26,47 @@ const parseData = (num, level, parentId = 0) => {
 const data = parseData(3, 1)
 console.log(`执行函数：parseData(40, 3)， 耗时 ${data} 秒`)
 
-export default data
+// export default data
 
-// export default [
-//   {
-//     id: '1',
-//     label: '陕西省',
-//     resource: { name: '陕西省' },
-//     children: [
-//       {
-//         id: '1-1', label: '西安市',
-//         resource: { name: '陕西省' },
-//         children: [
-//           { id: '1-1-1', label: '雁塔区' },
-//           { id: '1-1-2', label: '长安区' }
-//         ]
-//       },
-//       {
-//         id: '1-2', label: '咸阳市',
-//         children: [
-//           {
-//             id: '1-2-1', label: '礼泉县',
-//             children: [
-//               { id: '1-2-1-1', label: '城关镇' },
-//               { id: '1-2-1-2', label: '烟霞镇' },
-//               { id: '1-2-1-3', label: '烽火镇' },
-//               { id: '1-2-1-4', label: '石潭镇' },
-//               { id: '1-2-1-5', label: '昭陵乡' },
-//             ]
-//           },
-//           { id: '1-2-2', label: '乾县' },
-//           { id: '1-2-3', label: '永寿县' }
-//         ]
-//       }
-//     ]
-//   },
-//   {
-//     id: '2',
-//     label: '四川省',
-//     // children: [
-//     //   { id: '2-1', label: '成都市' },
-//     //   { id: '2-2', label: '汶川市' }
-//     // ]
-//   }
-// ]
+export default [
+  {
+    id: '1',
+    label: '陕西省',
+    resource: { name: '陕西省' },
+    children: [
+      {
+        id: '1-1', label: '西安市',
+        resource: { name: '陕西省' },
+        children: [
+          { id: '1-1-1', label: '雁塔区' },
+          { id: '1-1-2', label: '长安区' }
+        ]
+      },
+      {
+        id: '1-2', label: '咸阳市',
+        children: [
+          {
+            id: '1-2-1', label: '礼泉县',
+            children: [
+              { id: '1-2-1-1', label: '城关镇' },
+              { id: '1-2-1-2', label: '烟霞镇' },
+              { id: '1-2-1-3', label: '烽火镇' },
+              { id: '1-2-1-4', label: '石潭镇' },
+              { id: '1-2-1-5', label: '昭陵乡' },
+            ]
+          },
+          { id: '1-2-2', label: '乾县' },
+          { id: '1-2-3', label: '永寿县' }
+        ]
+      }
+    ]
+  },
+  {
+    id: '2',
+    label: '四川省',
+    // children: [
+    //   { id: '2-1', label: '成都市' },
+    //   { id: '2-2', label: '汶川市' }
+    // ]
+  }
+]
