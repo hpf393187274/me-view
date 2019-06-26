6d25533ae72114a753d6d4766d6b3cee45e7f6ad
hpf9527 <393187274@qq.com>
2019-06-26 17:31:12 +0800

    修改 icon  Symbol 为 Font class

--- src/components/basic/Icon.vue
+++ src/components/basic/Icon.vue
@@ -1,7 +1,8 @@
 <template>
-  <svg :title="title" @click="$emit('click')" aria-hidden="true" class="me-icon">
+  <!-- <svg :title="title" @click="$emit('click')" aria-hidden="true" class="me-icon">
     <use :xlink:href="`#${$slots.default[0].text}`"></use>
-  </svg>
+  </svg>-->
+  <i :class="`iconfont ${$slots.default[0].text} me-icon `" :title="title" @click="$emit('click')"/>
 </template>
 
 <script>
