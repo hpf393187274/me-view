524db1b7d4f23c5f7b8d8fc0cba9a2dd2759332e
hpf9527 <393187274@qq.com>
2020-04-21 22:16:15 +0800

    格式调整

--- example/components/index.js
+++
@@ -1,9 +0,0 @@
-import IconList from './IconList'
-const components = [ IconList ]
-export default {
-  install (Vue) {
-    for (const item of components) {
-      Vue.component(item.name, item)
-    }
-  }
-}
--- src/components/demo-block/index.js
+++
@@ -1,6 +0,0 @@
-import DemoBlock from './DemoBlock'
-
-DemoBlock.install = Vue => {
-  Vue.component(DemoBlock.name, DemoBlock)
-}
-export default DemoBlock
