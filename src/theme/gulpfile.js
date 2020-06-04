4f18ec370dcd07a40623be614e6e390cd38ded4f
hpf9527 <393187274@qq.com>
2020-06-04 16:56:30 +0800

    样式调整

--- src/theme/gulpfile.js
+++ src/theme/gulpfile.js
@@ -1,11 +1,13 @@
 'use strict'
 
 const { series, src, dest } = require('gulp')
+var header = require('gulp-header')
 const sass = require('gulp-sass')
 const cssmin = require('gulp-cssmin')
 
 function compile () {
   return src('./src/*.scss')
+    .pipe(header('@import \'../var.scss\';'))
     .pipe(sass.sync())
     .pipe(cssmin())
     .pipe(dest('./lib'))
