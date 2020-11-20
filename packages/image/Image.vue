797f74cecc554f622199cbc0b9f5feaf44f4300f
不一样的 <7960273+byyd@user.noreply.gitee.com>
2020-11-20 16:37:58 +0800

    图片放大调整

--- packages/image/Image.vue
+++ packages/image/Image.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="me-image">
     <img :src="valueInner" @click="visible = true"/>
-    <me-modal
-      draggable
-      closable-modal
+    <me-dialog
       v-if="preview"
-      :z-index="10000"
+      title="图片预览"
       class-container="image-modal"
+      hidden-footer
       v-model="visible"
     >
       <me-image ref="imageModel" class="me-flex" :value="valueInner" />
-      <div class="image-action-container">
+      <!-- <div class="image-action-container">
         <me-row class="image-action me-center">
           <me-icon v-if="zoom" @click="handlerScale(true)" title="放大">icon-plus_strong</me-icon>
           <me-icon v-if="zoom" @click="handlerScale(false)" title="缩小">icon-minus_strong</me-icon>
           <me-icon @click="visible = false" title="关闭">icon-cross</me-icon>
         </me-row>
-      </div>
-    </me-modal>
+      </div> -->
+    </me-dialog>
   </div>
 </template>
 <script>
